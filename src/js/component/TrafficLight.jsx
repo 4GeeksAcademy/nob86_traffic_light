84ae8c56f22e9e10f39bfb14a5b8009ed882e492
nob86 <gustavoadallacroce@gmail.com>
2024-09-21 20:32:15 +0000

    semaforo

--- src/js/component/TrafficLight.jsx
+++ src/js/component/TrafficLight.jsx
@@ -6,9 +6,10 @@ const TrafficLight = () => {
 
 	const [color, setColor] = useState("")
 	return (
-		<div>
-		<button className= {`bg-danger ${color === 'rojo' ? "light1" : ""}`} onClick={() => setColor('rojo')}></button>
-		<button className= {`bg-warning ${color === 'amarillo' ? "light2" : ""}`} onClick={() => setColor('amarillo')}></button>
+		
+		<div className="container bg-dark d-flex flex-column align-items-center p-4 mt-5">
+		<button className= {`bg-danger mb-3 ${color === 'rojo' ? "light1" : ""}`} onClick={() => setColor('rojo')}></button>
+		<button className= {`bg-warning mb-3 ${color === 'amarillo' ? "light2" : ""}`} onClick={() => setColor('amarillo')}></button>
 		<button className= {`bg-success ${color === 'verde' ? "light3" : ""}`} onClick={() => setColor('verde')}></button>
 		</div>
 	);
