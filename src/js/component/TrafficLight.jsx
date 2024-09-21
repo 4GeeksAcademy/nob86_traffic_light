c16fa7272f01a49b4231e99fbd3539c23d3e1176
nob86 <gustavoadallacroce@gmail.com>
2024-09-21 22:24:14 +0000

    widht

--- src/js/component/TrafficLight.jsx
+++ src/js/component/TrafficLight.jsx
@@ -1,18 +1,31 @@
-import React, {useState} from "react";
-
+import React, { useState } from "react";
 
 //create your first component
 const TrafficLight = () => {
+  const alternativColor = () => {
+    if (color === "rojo") setColor("amarillo");
+  };
 
-	const [color, setColor] = useState("")
-	return (
-		
-		<div className="container bg-dark d-flex flex-column align-items-center p-4 mt-5">
-		<button className= {`bg-danger mb-3 ${color === 'rojo' ? "light1" : ""}`} onClick={() => setColor('rojo')}></button>
-		<button className= {`bg-warning mb-3 ${color === 'amarillo' ? "light2" : ""}`} onClick={() => setColor('amarillo')}></button>
-		<button className= {`bg-success ${color === 'verde' ? "light3" : ""}`} onClick={() => setColor('verde')}></button>
-		</div>
-	);
+  const [color, setColor] = useState("");
+  return (
+    <div className="container bg-dark d-flex flex-column align-items-center p-4 mt-5">
+      <div>
+        <button
+          className={`bg-danger mb-3 ${color === "rojo" ? "light1" : ""}`}
+          onClick={() => setColor("rojo")}
+        ></button>
+        <button
+          className={`bg-warning mb-3 ${color === "amarillo" ? "light2" : ""}`}
+          onClick={() => setColor("amarillo")}
+        ></button>
+        <button
+          className={`bg-success ${color === "verde" ? "light3" : ""}`}
+          onClick={() => setColor("verde")}
+        ></button>
+      </div>
+	  <div></div>
+    </div>
+  );
 };
 
 export default TrafficLight;
